fdca5b02ff8e8d041a0dc4904130ffa9a7830b86
gil19443 <gil19443@uvg.edu.gt>
2021-03-25 17:39:42 -0600

    Update Laboratorio5_Digital2.ino
    comentarios finales

--- Laboratorio5_Digital2/Laboratorio5_Digital2.ino
+++ Laboratorio5_Digital2/Laboratorio5_Digital2.ino
@@ -1,26 +1,4 @@
-/*
-  Listfiles
- 
- This example shows how print out the files in a 
- directory on a SD card 
- 	
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
 
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 2 Feb 2014
- by Scott Fitzgerald
- 
- This example code is in the public domain.
-
- */
 #include <SPI.h>
 #include <SD.h>
 
